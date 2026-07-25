d6d2c56dbe769b4f21e4d5ee6bde17748de42d37
Gamnraa <ltaz1466@gmail.com>
2026-07-25 10:39:01 -0400

    Fixes

--- gmtk-2026/Scripts/Global.gd
+++ gmtk-2026/Scripts/Global.gd
@@ -13,63 +13,63 @@ var is_gameover = false
 
 
 var levels = [
-	"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
-	"16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"
+    "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15",
+    "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"
 ]
 
 func _ready():
-	var root = get_tree().get_root()
-	TheScene = root.get_child(root.get_child_count() - 1)
-	ThePlayer = TheScene.get_node("Player")
-	
-	
-	
-	await get_tree().create_timer(.1).timeout
-	is_ingame = true
-	
-	#TheScene.appear()
-	var next_level = load("res://Nodes/Levels/debug_level.tscn")
-	
-	if TheScene.get_node("Level").get_child(0): TheScene.get_node("Level").get_child(0).free()
-	TheScene.get_node("Level").add_child(next_level.instantiate())
+    var root = get_tree().get_root()
+    TheScene = root.get_child(root.get_child_count() - 1)
+    ThePlayer = TheScene.get_node("Player")
+    
+    
+    
+    await get_tree().create_timer(.1).timeout
+    is_ingame = true
+    
+    #TheScene.appear()
+    var next_level = load("res://Nodes/Levels/debug_level.tscn")
+    
+    if TheScene.get_node("Level").get_child(0): TheScene.get_node("Level").get_child(0).free()
+    TheScene.get_node("Level").add_child(next_level.instantiate())
 
 func level_transition():
-	#score is updated in MainScene since we've already increased the time by the time this gets called
-	level += 1
-	TheScene.get_node("Level").get_child(0).free()
-	var next_level = load("res://Nodes/Levels/debug_level2.tscn")
-	TheScene.get_node("Level").add_child(next_level.instantiate())
+    #score is updated in MainScene since we've already increased the time by the time this gets called
+    level += 1
+    TheScene.get_node("Level").get_child(0).free()
+    var next_level = load("res://Nodes/Levels/debug_level2.tscn")
+    TheScene.get_node("Level").add_child(next_level.instantiate())
 
 func reset_player():
-	ThePlayer.position = ThePlayer.start_pos
-	ThePlayer.velocity = Vector2()
-	deaths += 1
-	
+    ThePlayer.position = ThePlayer.start_pos
+    ThePlayer.velocity = Vector2()
+    deaths += 1
+    
 func end_game(won):
-	score = floor(max(0, score / (deaths + 1)))
-	if won: TheScene.get_node("EndScreen").get_node("Message").text = "You win!"
-	else: TheScene.get_node("EndScreen").get_node("Message").text = "You lose!"
-	
-	TheScene.get_node("EndScreen").get_node("Score").text = "Final Score: " + str(score)
-	TheScene.get_node("EndScreen").visible = true
-	
-	is_gameover = true
-	
+    score = floor(max(0, score / (deaths + 1)))
+    if won: TheScene.get_node("EndScreen").get_node("Message").text = "You win!"
+    else: TheScene.get_node("EndScreen").get_node("Message").text = "You lose!"
+    
+    TheScene.get_node("EndScreen").get_node("Score").text = "Final Score: " + str(score)
+    TheScene.get_node("EndScreen").visible = true
+    
+    is_gameover = true
+    
 func restart_game():
-	var next_level = load("res://Nodes/Levels/debug_level.tscn")
-	
-	if TheScene.get_node("Level").get_child(0): TheScene.get_node("Level").get_child(0).free()
-	TheScene.get_node("Level").add_child(next_level.instantiate())
-	
-	level = 0
-	TimeLeft = 15
-	score = 0
-	deaths = 0
-	is_ingame = true
-	is_gameover = false
-	TheScene.get_node("EndScreen").visible = false
-	ThePlayer.position = ThePlayer.start_pos
-	ThePlayer.velocity = Vector2()
-	
-	
-	
+    var next_level = load("res://Nodes/Levels/debug_level.tscn")
+    
+    if TheScene.get_node("Level").get_child(0): TheScene.get_node("Level").get_child(0).free()
+    TheScene.get_node("Level").add_child(next_level.instantiate())
+    
+    level = 0
+    TimeLeft = 15
+    score = 0
+    deaths = 0
+    is_ingame = true
+    is_gameover = false
+    TheScene.get_node("EndScreen").visible = false
+    ThePlayer.position = ThePlayer.start_pos
+    ThePlayer.velocity = Vector2()
+    
+    
+    
--- gmtk-2026/Scripts/Trap.gd
+++ gmtk-2026/Scripts/Trap.gd
@@ -5,7 +5,8 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_on_cooldown_timeout()
+	pass
+	#_on_cooldown_timeout()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
