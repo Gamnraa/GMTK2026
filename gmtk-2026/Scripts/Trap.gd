fa146a399281ab71244268a0528ceef58e67ccdd
Gamnraa <ltaz1466@gmail.com>
2026-07-24 18:56:49 -0700

    Fire trap

--- gmtk-2026/Scripts/Trap.gd
+++ gmtk-2026/Scripts/Trap.gd
@@ -16,3 +16,17 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		Global.reset_player()
+
+
+func _on_cooldown_timeout() -> void:
+	await get_tree().create_timer(.5).timeout
+	$Active.start()
+	$Area2D.monitoring = true
+	#play anim
+
+
+func _on_active_timeout() -> void:
+	$Cooldown.start()
+	$Area2D.monitoring = false
+	#unplay anim
+	
