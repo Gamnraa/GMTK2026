a3c859818f3153c12e9fd71f6aae799503049cac
Gamnraa <ltaz1466@gmail.com>
2026-07-24 19:09:22 -0700

    Update Trap.gd

--- gmtk-2026/Scripts/Trap.gd
+++ gmtk-2026/Scripts/Trap.gd
@@ -5,7 +5,7 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_on_cooldown_timeout()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
